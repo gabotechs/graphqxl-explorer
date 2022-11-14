179abb332b834f38b475eba016698b31b26b4f54
gabotechs <gabimtme@hotmail.com>
2022-11-14 18:52:23 +0100

    chore: logs

--- prebuild.sh
+++ prebuild.sh
@@ -5,7 +5,9 @@ set -eu
 cp node_modules/node-graphqxl/graphqxl public/graphqxl
 
 if ! yum &> /dev/null; then
+  echo "yum is not installed, omitting..."
   exit 0
 fi
 
+  echo "yum is available, installing dependencies..."
 yum install libuuid-devel libmount-devel
